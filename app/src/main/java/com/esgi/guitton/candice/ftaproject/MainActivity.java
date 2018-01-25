b8d97eace4bb11d407fe3190e97a08a67a2969d4
Unknown <candice.guitton@hotmail.fr>
2018-01-25 18:12:41 +0100

    bottom navigation menu + fragments

--- app/src/main/java/com/esgi/guitton/candice/ftaproject/MainActivity.java
+++ app/src/main/java/com/esgi/guitton/candice/ftaproject/MainActivity.java
@@ -1,13 +1,43 @@
 package com.esgi.guitton.candice.ftaproject;
 
+import android.support.annotation.NonNull;
+import android.support.design.internal.BottomNavigationMenu;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottom_navigation_menu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()) {
+
+                    case R.id.onglet_1:
+                    return true;
+                    case R.id.onglet_2:
+                        //Action quand onglet 2 sélectionné
+                        break;
+
+                    default:
+                        //Action quand onglet 3 sélectionné
+
+                        break;
+                }
+                return false;
+            };
+
+        });
     }
 }
