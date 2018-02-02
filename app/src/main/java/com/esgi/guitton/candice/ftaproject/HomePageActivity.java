212bbd8b43f99be97a9ad365ad0bdb5933232018
Unknown <candice.guitton@hotmail.fr>
2018-02-02 16:57:02 +0100

    Revert "avancement"
    This reverts commit 7918b5451585c3573faf6165918661b22cf8d2e5.

--- app/src/main/java/com/esgi/guitton/candice/ftaproject/HomePageActivity.java
+++ app/src/main/java/com/esgi/guitton/candice/ftaproject/HomePageActivity.java
@@ -1,33 +1,13 @@
 package com.esgi.guitton.candice.ftaproject;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 
 public class HomePageActivity extends AppCompatActivity {
-    EditText edit_login;
-    EditText edit_password;
-    Button buttonConnect;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
-
-
-        edit_login = findViewById(R.id.edit_login);
-        edit_password = findViewById(R.id.edit_password);
-        buttonConnect = findViewById(R.id.connexion_button);
-
-        buttonConnect.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(HomePageActivity.this, MainActivity.class);
-                startActivity(intent);            }
-        });
-        //connexion avec
-
     }
 }
