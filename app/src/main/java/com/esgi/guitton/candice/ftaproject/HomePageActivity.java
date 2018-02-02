b1cdf73cc60af01b9c7ba927454a9296ba778ecc
Unknown <candice.guitton@hotmail.fr>
2018-02-02 19:08:36 +0100

    google sign in

--- app/src/main/java/com/esgi/guitton/candice/ftaproject/HomePageActivity.java
+++ app/src/main/java/com/esgi/guitton/candice/ftaproject/HomePageActivity.java
@@ -1,13 +1,111 @@
 package com.esgi.guitton.candice.ftaproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class HomePageActivity extends AppCompatActivity {
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class HomePageActivity extends AppCompatActivity implements View.OnClickListener {
+    /*EditText edit_login;
+    EditText edit_password;
+    */
+
+    private GoogleSignInClient mGoogleSignInClient;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        ButterKnife.bind(this);
+
+        SignInButton signInButton = findViewById(R.id.sign_in_button);
+        findViewById(R.id.sign_in_button).setOnClickListener(this);
+
+      //  edit_login = findViewById(R.id.edit_login);
+      //  edit_password = findViewById(R.id.edit_password);
+
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+
+        if (account != null){
+            System.out.println("account :"+ account.getDisplayName());
+            System.out.println("account :"+ account.getEmail());
+            System.out.println("account :"+ account.getFamilyName());
+            launchIntent();
+        }
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == 1000) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            System.out.println("account :"+ account.getDisplayName());
+            System.out.println("account :"+ account.getEmail());
+            System.out.println("account :"+ account.getFamilyName());
+            launchIntent();
+            // Signed in successfully, show authenticated UI.
+            //updateUI(account);
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w("Home", "signInResult:failed code=" + e.getStatusCode());
+            //updateUI(null);
+        }
+    }
+
+    void launchIntent(){
+        Intent intent = new Intent(HomePageActivity.this, MainActivity.class);
+        startActivity(intent);
+
+    }
+
+    /*@OnClick(R.id.connexion_button)
+    public void onConnexionButton(){
+    }*/
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.sign_in_button:
+                signIn();
+                break;
+        }
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, 1000);
     }
 }
